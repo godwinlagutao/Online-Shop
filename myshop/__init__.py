b30ea5a326ae84b49fe02ff38522124135bf9895
godwinlagutao <generationsecond2020@gmail.com>
2024-11-16 22:54:59 +0100

    fully designed asynchronous tasks with rabbitmq, celery and flower

--- myshop/__init__.py
+++ myshop/__init__.py
@@ -3,5 +3,6 @@ from .celery import app as celery_app
 
 __all__ = ['celery_app']
 
+# docker pull rabbitmq
 # sudo docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672
 # celery -A myshop worker -1 info
